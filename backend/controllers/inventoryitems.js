d9a7384618fd09293d26d4fa51236ad7c1094748
drios2023-ga <drios111-general@yahoo.com>
2023-07-25 16:16:04 -0500

    converted working API to restful routes and controller functions

--- backend/controllers/inventoryitems.js
+++ backend/controllers/inventoryitems.js
@@ -1,12 +1,70 @@
 const inventory = require('../models/inventoryitem');
 
 module.exports = {
-    getAll
+    getAll,
+    show,
+    new: newItem,
+    update,
+    delete: deleteItem
 
 }
 
 async function getAll(req, res){
-    await inventory.find();
-    console.log(inventory);
-    //res.json(inventory);
+
+	try {
+        // get all items
+        res.json(await inventory.find({}));
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
+
+}
+
+async function show (req, res) {
+
+    try {
+        // send one item
+        console.log(req.params.id);
+        res.json(await inventory.findById(req.params.id));
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
+}
+
+async function newItem(req, res) {
+
+    try {
+        // create new item
+        res.json(await inventory.create(req.body));
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
+}
+
+async function update(req, res){
+
+    try {
+        // update item by ID
+        res.json(
+          await inventory.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        );
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
+
+}
+
+async function deleteItem (req,res){
+
+    try {
+        // delete item by ID
+        res.json(await inventory.findByIdAndRemove(req.params.id));
+      } catch (error) {
+        //send error
+        res.status(400).json(error);
+      }
 }
--- backend/routes/inventoryitems.js
+++ backend/routes/inventoryitems.js
@@ -3,64 +3,82 @@ var router = express.Router();
 inventoryCtrl = require('../controllers/inventoryitems')
 const inventory = require('../models/inventoryitem');
 
+//get all items
+router.get('/', inventoryCtrl.getAll);
+
+//get one item
+router.get('/:id', inventoryCtrl.show);
+
+//add new
+router.post('/new', inventoryCtrl.new);
+
+//update
+router.put("/:id/update", inventoryCtrl.update);
+
+//delete
+router.delete("/:id", inventoryCtrl.delete);
+
 
 //get all
-router.get("/", async (req, res) => {
+// router.get("/", async (req, res) => {
 
-	try {
-        // get all items
-        res.json(await inventory.find({}));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
-})
+// 	try {
+//         // get all items
+//         res.json(await inventory.find({}));
+//       } catch (error) {
+//         //send error
+//         res.status(400).json(error);
+//       }
+// })
+
 
 //get one
-router.get("/:id", async (req, res) => {
-    try {
-        // send one item
-        console.log(req.params.id);
-        res.json(await inventory.findById(req.params.id));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
-});
+// router.get("/:id", async (req, res) => {
+//     try {
+//         // send one item
+//         console.log(req.params.id);
+//         res.json(await inventory.findById(req.params.id));
+//       } catch (error) {
+//         //send error
+//         res.status(400).json(error);
+//       }
+// });
 
 //insert
-router.post("/", async (req, res) => {
-    try {
-      // create new item
-      res.json(await inventory.create(req.body));
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
+// router.post("/", async (req, res) => {
+//     try {
+//       // create new item
+//       res.json(await inventory.create(req.body));
+//     } catch (error) {
+//       //send error
+//       res.status(400).json(error);
+//     }
+//   });
 
 
 //update
-router.put("/:id", async (req, res) => {
-    try {
-      // update item by ID
-      res.json(
-        await inventory.findByIdAndUpdate(req.params.id, req.body, {new:true})
-      );
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
+// router.put("/:id", async (req, res) => {
+//     try {
+//       // update item by ID
+//       res.json(
+//         await inventory.findByIdAndUpdate(req.params.id, req.body, {new:true})
+//       );
+//     } catch (error) {
+//       //send error
+//       res.status(400).json(error);
+//     }
+//   });
   
 //delete
-  router.delete("/:id", async (req, res) => {
-    try {
-      // delete item by ID
-      res.json(await inventory.findByIdAndRemove(req.params.id));
-    } catch (error) {
-      //send error
-      res.status(400).json(error);
-    }
-  });
+//   router.delete("/:id", async (req, res) => {
+//     try {
+//       // delete item by ID
+//       res.json(await inventory.findByIdAndRemove(req.params.id));
+//     } catch (error) {
+//       //send error
+//       res.status(400).json(error);
+//     }
+//   });
+
+
 module.exports = router;
