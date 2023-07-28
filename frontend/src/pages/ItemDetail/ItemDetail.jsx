c45b9b3a54c1f142963ec82f4c472108038b7014
drios2023-ga <drios111-general@yahoo.com>
2023-07-27 19:08:35 -0500

    plus minus one is working

--- frontend/src/pages/ItemDetail/ItemDetail.jsx
+++ frontend/src/pages/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,40 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { deleteItem } from "../../utilities/items-service";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { getOneItem, updateItem, deleteItem } from "../../utilities/items-service";
+import { useEffect, useState } from "react";
 
 export default function ItemDetail({ getItem }) {
-  const location = useLocation();
-  const { item } = location.state;
-  const navigate = useNavigate();
 
+  const { id } = useParams();
+  const [item, setItem] = useState({});
+  const navigate = useNavigate();
+  
   async function removeItem(id) {
     await deleteItem(id);
     getItem();
     navigate("/");
   }
+
+  async function plusMinusOne(changeValue){
+    const updatedItemData = {
+      _id: item._id,
+      productname: item.productname,
+      SKU: item.SKU,
+      quantity: item.quantity+changeValue,
+    }
+
+    console.log(updatedItemData);
+    await updateItem(updatedItemData);
+    fetchOneItem();
+  }
+
+  async function fetchOneItem(){
+    let oneItem = await getOneItem(id);
+    setItem(oneItem);
+  }
+
+  useEffect(()=>{
+    fetchOneItem();
+  },[])
 
   return (
     <div className="item-detail">
@@ -24,6 +47,13 @@ export default function ItemDetail({ getItem }) {
       </Link>{" "}
       <span> &nbsp; | &nbsp; </span>
       <button onClick={() => removeItem(item._id)}>Delete</button>
+      <br></br>
+      <br></br>
+
+    <button onClick={() => plusMinusOne(1)}>+1</button>
+    <span> &nbsp;&nbsp; </span>
+    <button onClick={() => plusMinusOne(-1)}>-1</button>
+ 
     </div>
   );
 }
