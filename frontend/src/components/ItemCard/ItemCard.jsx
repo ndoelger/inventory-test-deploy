646cf857402a9fe7d8fe6d6d7e7d752462027d9c
bryanstark1 <bryandstark@icloud.com>
2023-07-28 16:02:33 -0400

    Plus Minus One function and buttons work in modal

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function ItemCard({item}) {
   return (
-    <Link to={`Modal`} state={{item: item}}>
+    <Link to={`Modal`} state={{currentItem: item}}>
       <li className='item-card'>
         <h2>{item.productname}</h2>
         <h3><span>Qty: {item.quantity}</span> | <span>SKU: {item.SKU}</span></h3>
